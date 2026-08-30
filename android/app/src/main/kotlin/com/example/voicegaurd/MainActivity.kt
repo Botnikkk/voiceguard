d92790d0b8204f565f7093f44f74c3d644237ec9
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2026-08-31 01:15:40 +0530

    mainActivity file corrected

--- android/app/src/main/kotlin/com/example/voicegaurd/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/voicegaurd/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.example.voiceguard
+package com.example.voicegaurd
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity()
+class MainActivity: FlutterActivity() {
+}
