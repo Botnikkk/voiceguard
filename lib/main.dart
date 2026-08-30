666d53e1b0a711634ef67c2d20fc60c3a9d29dc2
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2026-08-31 00:03:33 +0530

    initilization of voicegaurd

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const VoiceGaurdApp());
+}
+
+class VoiceGaurdApp extends StatelessWidget {
+  const VoiceGaurdApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
