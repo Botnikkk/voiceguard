98e9637f39a4a3d86d3615d7338dff21d03fd649
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2026-09-04 20:15:31 +0530

    api config made http safe.

--- lib/core/network/api_config.dart
+++ lib/core/network/api_config.dart
@@ -2,5 +2,5 @@ class ApiConfig {
   ApiConfig._();
 
   static const String voiceAnalysisWsUrl =
-      'ws://relieving-alone-rewind.ngrok-free.dev/ws/detect';
+      'wss://relieving-alone-rewind.ngrok-free.dev/ws/detect';
 }
